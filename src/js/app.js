9c742c7e135630daed561a7b38b434a24e2535bf
jakwuh <jakwuh@gmail.com>
2016-03-17 20:39:01 +0300

    Add Promise polyfill for IE

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import Tasks from './collections/tasks'
 import TasksView from './views/tasks'
+import promise from 'es6-promise'
 
 (()=>{
+	promise.polyfill()
 	document.body.querySelector('#container').appendChild(
 		(new TasksView({collection: new Tasks})).render().el)
 })()
